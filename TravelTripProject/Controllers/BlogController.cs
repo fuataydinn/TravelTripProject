eec4fb11557f90986e2540e2e568c781f21a7867
Fuat Aydin <81709694+fuataydinn@users.noreply.github.com>
2022-03-10 15:43:28 +0300

    updated 10/03/2022

---
+++ TravelTripProject/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Models.Siniflar;
+
+namespace TravelTripProject.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
+        }
+        public ActionResult BlogDetay(int id)
+        {
+            var blogBul = c.Blogs.Where(x => x.ID == id).ToList();
+            return View(blogBul);
+        }
+    }
+}
